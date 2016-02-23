2ca38bbd488c3a456c9c99c54f6c10d534b66e61
Diarmuid Murphy <diarmuid.com@gmail.com>
2016-02-23 00:05:26 +0000

    comments for clarity

--- sqltest.php
+++ sqltest.php
@@ -6,6 +6,7 @@ if($conn->connect_error) {
 	die($conn->connect_error);
 }
 
+// if delete and isbn (because isbn is the primary key) are POSTed look up and remove this row. PS - these are hidden fields in the form.
 if(isset($_POST["delete"]) && isset($_POST["isbn"])) {
 	$isbn = get_post($conn, "isbn");
 	$query = "DELETE FROM classics WHERE isbn='$isbn'";
@@ -15,18 +16,21 @@ if(isset($_POST["delete"]) && isset($_POST["isbn"])) {
 	}
 }
 
+// make sure all fields are set
 if( isset($_POST["author"]) &&
 	isset($_POST["title"]) &&
 	isset($_POST["category"]) &&
 	isset($_POST["year"]) &&
 	isset($_POST["isbn"])) {
 
+	// then satitize the input using self defined function get_post
 	$author = get_post($conn, "author");
 	$title = get_post($conn, "title");
 	$category = get_post($conn, "category");
 	$year = get_post($conn, "year");
 	$isbn = get_post($conn, "isbn");
 
+	// add form inputs to classics table
 	$query = "INSERT INTO classics VALUES" . "('$author', '$title', '$category', '$year', '$isbn')";
 	$result = $conn->query($query);
 
@@ -55,6 +59,7 @@ if(!$result) {
 
 $rows = $result->num_rows;
 
+// iterate though entire form, printing out all details along with alredy loaded (isbn) delete buttons.
 for($j=0; $j < $rows; ++$j) {
 	$result->data_seek($j);
 	$row = $result->fetch_array(MYSQLI_NUM);
